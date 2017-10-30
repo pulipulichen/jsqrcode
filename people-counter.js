0caada3c42bdb81f4afd46ef6bcf58792689caa3
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-10-30 20:09:26 +0800

    1030 好，我做到那了？

--- people-counter.js
+++ people-counter.js
@@ -1,3 +1,26 @@
 $(function () {
-    load();
+    load(function (_message) {
+        var _timestamp = get_timestamp();
+        //console.log(_timestamp + " " + _message);
+        push_to_log(_timestamp + " " + _message);
+    });
 });
+
+var get_timestamp = function () {
+    var d = new Date;
+    var dformat = [d.getMonth()+1,
+               d.getDate()].join('/')+' '+
+              [d.getHours(),
+               d.getMinutes(),
+               d.getSeconds()].join(':');
+    return dformat;
+};
+
+var max_log_display = 10;
+var logs = [];
+var push_to_log = function (_log) {
+    logs.push(_log);
+    var _show_log = JSON.parse(JSON.stringify(logs));
+    _show_log.reverse();
+    $("#log").html(_show_log.join("\n"));
+};
