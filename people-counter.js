68098560e9329d32e78cdb4838c3cb86aca8c9ea
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-10-30 20:20:13 +0800

    1030 設定

--- people-counter.js
+++ people-counter.js
@@ -3,6 +3,18 @@ $(function () {
         var _timestamp = get_timestamp();
         //console.log(_timestamp + " " + _message);
         push_to_log(_timestamp + " " + _message);
+        
+        // -------------------
+        
+        if (_message === false) {
+            $('#counter_enable').attr('disabled', 'disabled');
+        }
+        else {
+            $('#counter_enable').removeAttr('disabled');
+        }
+        
+        // -------------------
+        
     });
 });
 
@@ -20,6 +32,11 @@ var max_log_display = 10;
 var logs = [];
 var push_to_log = function (_log) {
     logs.push(_log);
+    
+    if (logs.length > max_log_display) {
+        logs.shift();
+    }
+    
     var _show_log = JSON.parse(JSON.stringify(logs));
     _show_log.reverse();
     $("#log").html(_show_log.join("\n"));
